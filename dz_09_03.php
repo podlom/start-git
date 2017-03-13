a6a93606bddab9ad67e1338c99d9bec5a22c63e9
Yuri <yuriy-k@bk.ru>
2017-03-13 20:30:03 +0200

    dz от 09.03.2017

---
+++ dz_09_03.php
@@ -0,0 +1,21 @@
+﻿
+<?php
+
+$flag = true;
+
+for($i = 2; $i <= 100; $i++) {
+
+for($j = 2; $j < $i; $j++) { //<$i
+if($i % $j == 0) {
+$flag = false;
+break;
+}
+}
+
+if($flag) echo $i. " ";
+$flag = true;
+}
+
+
+
+
