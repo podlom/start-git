948c9beb234525f965c3c8c3c340f6c752d6eb50
Shkodenko V. Taras <podlom@users.noreply.github.com>
2017-03-22 19:02:21 +0200

    Update 4.php

--- arrays_loops_tasks/4.php
+++ arrays_loops_tasks/4.php
@@ -1,22 +1,18 @@
 ﻿<?php
 
-		$arr = array('green'=>'зеленый', 'red'=>'красный', 'blue'=>'голубой');
-      
-            foreach ($arr as $k => $v) {
-	        echo '$arr [' .$k .']: <br>';
-		  }
-		    echo '<hr>';
-			foreach ($arr as $k => $v) {
-	        echo '$arr [' .$v .']: <br>';
-	
-	      }
-		
-	  
-	 
-	  
-	  
-	  
-	  
-	 
-	
+/**
+ * 4. Дан массив $arr. С помощью первого цикла foreach выведите на экран столбец ключей,
+ * с помощью второго — столбец элементов.
+ */
 
+$arr = array('green'=>'зеленый', 'red'=>'красный', 'blue'=>'голубой');
+      
+foreach ($arr as $k => $v) {
+	echo $k . '<br>';
+}
+
+echo '<hr>';
+
+foreach ($arr as $k => $v) {
+	echo $v . '<br>';
+}
