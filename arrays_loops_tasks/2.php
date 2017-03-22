c8f78c56631dc400a834c44ce308ecfefb6766df
Shkodenko V. Taras <podlom@users.noreply.github.com>
2017-03-22 11:02:51 +0200

    Update 2.php
    Согласно документации http://php.net/manual/ru/function.array-sum.php не нужен цикл

--- arrays_loops_tasks/2.php
+++ arrays_loops_tasks/2.php
@@ -1,12 +1,6 @@
 ﻿<?php
       
-      $arr = array(1, 20, 15, 17, 24, 35);
+$arr = array(1, 20, 15, 17, 24, 35);
 	   
-    foreach ($arr as $item) {
-	$result = array_sum($arr);
-	echo $result;
-	break;
-	
-	}
-	
-
+$result = array_sum($arr);
+echo $result;
